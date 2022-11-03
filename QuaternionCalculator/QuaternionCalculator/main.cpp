331cdf94b1caf40fe8dc09eafd2f10c5035fcade
Erik Johansson <erikjohansson1999@outlook.co.nz>
2022-11-03 18:06:28 +1300

    Read File Contents as Strings

--- QuaternionCalculator/QuaternionCalculator/main.cpp
+++ QuaternionCalculator/QuaternionCalculator/main.cpp
@@ -9,17 +9,18 @@ int main()
 	std::string a;
 	std::string b;
 	std::string scalar;
+	std::string trash;
 
 	// setting values from the file
 	if (myfile.is_open())
 	{
-		
 		std::getline(myfile, a);
+		std::getline(myfile, trash); // blank line in file
 		std::getline(myfile, b);
+		std::getline(myfile, trash); // blank line in file
 		std::getline(myfile, scalar);
 
 		myfile.close();
-
 	}
 	else
 	{
@@ -28,7 +29,7 @@ int main()
 
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
-	std::cout << scalar << std::endl;
+	std::cout << scalar << std::endl << std::endl;
 
 	system("PAUSE");
 
