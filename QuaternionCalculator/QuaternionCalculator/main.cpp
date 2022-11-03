d5fc7f42599b4c477007900f5bfab885206ad374
Erik Johansson <erikjohansson1999@outlook.co.nz>
2022-11-03 15:55:09 +1300

    Added File Loading

--- QuaternionCalculator/QuaternionCalculator/main.cpp
+++ QuaternionCalculator/QuaternionCalculator/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
-#include <string>
+#include <fstream>
 
 int main()
 {
+	std::ifstream myfile("Quaternion.txt"); // load file
+
+	int a = 0;
+
+	// setting values from the file
+	if (myfile.is_open())
+	{
+		myfile >> a;
+
+		myfile.close();
+
+	}
+	else
+	{
+		std::cout << "ERROR LOADING FILE!!! \n\n";
+	}
+
+	std::cout << a;
+
+	system("PAUSE");
+
 	return 0;
 }
