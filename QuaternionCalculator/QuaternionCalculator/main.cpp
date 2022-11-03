33d293a3de4889e54fbf111491e2613736347aaa
Erik Johansson <erikjohansson1999@outlook.co.nz>
2022-11-04 08:50:55 +1300

    Updated things

--- QuaternionCalculator/QuaternionCalculator/main.cpp
+++ QuaternionCalculator/QuaternionCalculator/main.cpp
@@ -10,8 +10,8 @@ int main()
 	std::string line;
 	std::string line2;
 	std::string trash;
-	float var1, var2, var3, var4;
-	float var5, var6, var7, var8, scalar;
+	float var1 = 0, var2 = 0, var3 = 0, var4 = 0;
+	float var5 = 0, var6 = 0, var7 = 0, var8 = 0, scalar = 0;
 	// setting values from the file
 	if (getline(myfile, line))
 	{
@@ -24,7 +24,18 @@ int main()
 		ss >> var1 >> symb1 >> var2 >> symb2 >> symb3 >> var3 >> symb4 >> symb5 >> var4 >> symb6 >> symb7;
 		std::cout << var1 << symb1 << var2 << symb2 << symb3 << var3 << symb4 << symb5 << var4 << symb6 << symb7 << std::endl << std::endl;
 
-
+		if (symb1 == "-")
+		{
+			var2 *= -1;
+		}
+		if (symb3 == "-")
+		{
+			var3 *= -1;
+		}
+		if (symb5 == "-")
+		{
+			var4 *= -1;
+		}
 	}
 	getline(myfile, trash);
 	if (getline(myfile, line2))
@@ -37,11 +48,26 @@ int main()
 
 		ss >> var5 >> symb1 >> var6 >> symb2 >> symb3 >> var7 >> symb4 >> symb5 >> var8 >> symb6 >> symb7;
 		std::cout << var5 << symb1 << var6 << symb2 << symb3 << var7 << symb4 << symb5 << var8 << symb6 << symb7 << std::endl << std::endl;
-		//
+		
+		if (symb1 == "-")
+		{
+			var6 *= -1;
+		}
+		if (symb3 == "-")
+		{
+			var7 *= -1;
+		}
+		if (symb5 == "-")
+		{
+			var8 *= -1;
+		}
 	}
 	std::getline(myfile, trash);
 	myfile >> scalar;
-	std::cout << scalar << std::endl;
+	std::cout << scalar << std::endl << std::endl;
+
+	
+	
 
 
 
