9b63fa82d25146d4c4a16fbb61533905aaf4ce92
Erik Johansson <erikjohansson1999@outlook.co.nz>
2022-11-04 08:59:26 +1300

    Update stuff

--- QuaternionCalculator/QuaternionCalculator/main.cpp
+++ QuaternionCalculator/QuaternionCalculator/main.cpp
@@ -10,8 +10,8 @@ int main()
 	std::string line;
 	std::string line2;
 	std::string trash;
-	float var1 = 0, var2 = 0, var3 = 0, var4 = 0;
-	float var5 = 0, var6 = 0, var7 = 0, var8 = 0, scalar = 0;
+	float a = 0, ai = 0, aj = 0, ak = 0;
+	float b = 0, bi = 0, bj = 0, bk = 0, scalar = 0;
 	// setting values from the file
 	if (getline(myfile, line))
 	{
@@ -21,20 +21,20 @@ int main()
 		std::string symb8, symb9, symb10, symb11, symb12, symb13, symb14;
 
 
-		ss >> var1 >> symb1 >> var2 >> symb2 >> symb3 >> var3 >> symb4 >> symb5 >> var4 >> symb6 >> symb7;
-		std::cout << var1 << symb1 << var2 << symb2 << symb3 << var3 << symb4 << symb5 << var4 << symb6 << symb7 << std::endl << std::endl;
+		ss >> a >> symb1 >> ai >> symb2 >> symb3 >> aj >> symb4 >> symb5 >> ak >> symb6 >> symb7;
+		std::cout << "a = " << line << std::endl << std::endl;
 
 		if (symb1 == "-")
 		{
-			var2 *= -1;
+			ai *= -1;
 		}
 		if (symb3 == "-")
 		{
-			var3 *= -1;
+			aj *= -1;
 		}
 		if (symb5 == "-")
 		{
-			var4 *= -1;
+			ak *= -1;
 		}
 	}
 	getline(myfile, trash);
@@ -46,25 +46,25 @@ int main()
 		std::string symb8, symb9, symb10, symb11, symb12, symb13, symb14;
 
 
-		ss >> var5 >> symb1 >> var6 >> symb2 >> symb3 >> var7 >> symb4 >> symb5 >> var8 >> symb6 >> symb7;
-		std::cout << var5 << symb1 << var6 << symb2 << symb3 << var7 << symb4 << symb5 << var8 << symb6 << symb7 << std::endl << std::endl;
+		ss >> b >> symb1 >> bi >> symb2 >> symb3 >> bj >> symb4 >> symb5 >> bk >> symb6 >> symb7;
+		std::cout << "b = " << line2 << std::endl << std::endl;
 		
 		if (symb1 == "-")
 		{
-			var6 *= -1;
+			bi *= -1;
 		}
 		if (symb3 == "-")
 		{
-			var7 *= -1;
+			bj *= -1;
 		}
 		if (symb5 == "-")
 		{
-			var8 *= -1;
+			bk *= -1;
 		}
 	}
 	std::getline(myfile, trash);
 	myfile >> scalar;
-	std::cout << scalar << std::endl << std::endl;
+	std::cout <<"scalar = " << scalar << std::endl << std::endl;
 
 	
 	
