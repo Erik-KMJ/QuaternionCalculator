d5a860c4e2bd14e841c3d4938470e512e2f04585
DogsAreHumansToo <marc_laban@hotmail.com>
2022-11-04 05:39:15 +1300

    Scuffed fix

--- QuaternionCalculator/QuaternionCalculator/main.cpp
+++ QuaternionCalculator/QuaternionCalculator/main.cpp
@@ -1,35 +1,49 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 int main()
 {
 	std::ifstream myfile("Quaternion.txt"); // load file
 
-	std::string a;
-	std::string b;
-	std::string scalar;
+	std::string line;
+	std::string line2;
 	std::string trash;
-
+	int var1, var2, var3, var4;
+	int var5, var6, var7, var8, scalar;
 	// setting values from the file
-	if (myfile.is_open())
+	if (getline(myfile, line))
 	{
-		std::getline(myfile, a);
-		std::getline(myfile, trash); // blank line in file
-		std::getline(myfile, b);
-		std::getline(myfile, trash); // blank line in file
-		std::getline(myfile, scalar);
+		std::istringstream ss(line);
 
-		myfile.close();
+		std::string symb1, symb2, symb3, symb4, symb5, symb6, symb7;
+		std::string symb8, symb9, symb10, symb11, symb12, symb13, symb14;
+
+
+		ss >> var1 >> symb1 >> var2 >> symb2 >> symb3 >> var3 >> symb4 >> symb5 >> var4 >> symb6 >> symb7;
+		std::cout << var1 << symb1 << var2 << symb2 << symb3 << var3 << symb4 << symb5 << var4 << symb6 << symb7 << std::endl << std::endl;
+
+
 	}
-	else
+	getline(myfile, trash);
+	if (getline(myfile, line2))
 	{
-		std::cout << "ERROR LOADING FILE!!! \n\n";
-	}
+		std::istringstream ss(line2);
 
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << scalar << std::endl << std::endl;
+		std::string symb1, symb2, symb3, symb4, symb5, symb6, symb7;
+		std::string symb8, symb9, symb10, symb11, symb12, symb13, symb14;
+
+
+		ss >> var5 >> symb1 >> var6 >> symb2 >> symb3 >> var7 >> symb4 >> symb5 >> var8 >> symb6 >> symb7;
+		std::cout << var5 << symb1 << var6 << symb2 << symb3 << var7 << symb4 << symb5 << var8 << symb6 << symb7 << std::endl << std::endl;
+		//
+	}
+	std::getline(myfile, trash);
+	myfile >> scalar;
+	std::cout << scalar;
+
+
 
 	
 
